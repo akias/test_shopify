cf6446f2a47a74388afba2b48b0f3511644e9413
alias <saika@toki.waseda.jp>
2019-10-02 15:21:28 +0900

    feat: add the resource picker

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f0924f8207395e16beac";
+/******/ 	var hotCurrentHash = "7e004d224728eb8d26f3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
